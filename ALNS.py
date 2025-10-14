e06302b66303df2e8a5fd5939076a7c80d6332d7
Matteo Meloni <matteo.meloni01@icloud.com>
2025-10-14 10:00:45 +0200

    Use greedy insertion for initial solution

--- ALNS.py
+++ ALNS.py
@@ -54,8 +54,8 @@ class ALNS:
         Method that constructs an initial solution using random insertion
         """
         self.currentSolution = Solution(self.problem,list(),list(),list(self.problem.requests.copy()))
-        self.currentSolution.executeRandomInsertion(self.randomGen)
-        #self.currentSolution.executeGreedyInsertion(self.randomGen)
+        #self.currentSolution.executeRandomInsertion(self.randomGen)
+        self.currentSolution.executeGreedyInsertion(self.randomGen)
         #self.currentSolution.executeRegretInsertion(self.randomGen)
         self.currentSolution.computeDistance()
         self.bestSolution = self.currentSolution.copy()
